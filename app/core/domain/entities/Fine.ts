53d06400024a6f7a25ddeee7316afdcbc568f364
Rowleen <julianpeacevedo@gmail.com>
2024-07-29 01:27:50 +0200

    Create fine status as a type

--- app/core/domain/entities/Fine.ts
+++ app/core/domain/entities/Fine.ts
@@ -1,6 +1,9 @@
+export type FineStatus = 'pending' | 'approved' | 'rejected'
+
 export type Fine = {
   readonly initiatorId: number
   readonly sender: number
   readonly recipen: number
   readonly tokensAmmount: number
+  status: FineStatus
 }
