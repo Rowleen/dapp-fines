783ed2fec24818bdab53a0bc5576bcfbd30e4dd2
Rowleen <julianpeacevedo@gmail.com>
2024-07-29 11:27:19 +0200

    Create button component

---
+++ app/components/Button/Button.tsx
@@ -0,0 +1,46 @@
+'use client'
+import { FC } from 'react'
+import classNames from 'classnames'
+
+import styles from './button.module.sass'
+
+interface ButtonProps {
+  color?: 'primary'
+  text: string
+  onClick?: (event: React.MouseEvent) => void
+  shape: 'pill' | 'button' | 'link'
+  value?: string
+  type: 'button' | 'submit' | 'reset' | undefined
+  disabled?: boolean
+}
+
+const Button: FC<ButtonProps> = ({
+  color,
+  text,
+  onClick,
+  shape,
+  value,
+  type,
+  disabled
+}) => {
+  const buttonClass = classNames({
+    [styles.button]: shape === 'button',
+    [styles.pill]: shape === 'pill',
+    [styles.link]: shape === 'link',
+    [styles.primary]: color === 'primary'
+  })
+
+  return (
+    <button
+      className={buttonClass}
+      type={type}
+      onClick={onClick}
+      value={value}
+      disabled={disabled}
+    >
+      {text}
+    </button>
+  )
+}
+
+export default Button
