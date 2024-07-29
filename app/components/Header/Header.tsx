d6f205e4c71c3f72031642d80ec6f6c17a38ba7e
Rowleen <julianpeacevedo@gmail.com>
2024-07-29 20:21:59 +0200

    Add navigation between views

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { FC } from 'react'
+import Link from 'next/link'
 
 import styles from './header.module.sass'
 
@@ -7,6 +8,9 @@ const Header: FC = () => {
   return (
     <header className={styles.header}>
       <h1>Dapp fines</h1>
+
+      <Link href={'/'}>List of fines</Link>
+      <Link href={'/create-fine'}>Create Fine</Link>
     </header>
   )
 }
