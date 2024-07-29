3c44653f4f4433a62004f76781f9d4271ffdb4e1
Rowleen <julianpeacevedo@gmail.com>
2024-07-29 15:04:03 +0200

    Add missing property: id

--- app/core/domain/entities/Fine.ts
+++ app/core/domain/entities/Fine.ts
@@ -1,6 +1,7 @@
 export type FineStatus = 'pending' | 'approved' | 'rejected'
 
 export type Fine = {
+  id: number
   readonly initiatorId: number
   readonly sender: number
   readonly recipent: number
