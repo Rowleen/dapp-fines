f0fec84de18148dc14563702059ac6c45d2d08b2
Rowleen <julianpeacevedo@gmail.com>
2024-07-29 18:30:11 +0200

    Delete unnecesary code

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,8 @@
 'use client'
-import { Header, FinesList } from './components'
-
-import styles from './sass/home.module.sass'
+import { FinesList } from './components'
 
 const Home = () => {
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.home}>
-        <Header />
-
-        <section className={styles.titleWrapper}>
-          <h1 className={styles.title}>The place to fine others</h1>
-
-          <h2 className={styles.subtitle}>Let&apos;s do new friends</h2>
-        </section>
-
-        <FinesList />
-      </div>
-    </div>
-  )
+  return <FinesList />
 }
 
 export default Home
