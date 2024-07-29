14b28501dde376266929f07a5371023a2e77cf21
Rowleen <julianpeacevedo@gmail.com>
2024-07-29 12:25:42 +0200

    Add avatar to the list

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from './Header/Header'
 export { default as Fine } from './Fine/Fine'
 export { default as FinesList } from './FinesList/FinesList'
+export { default as Avatar } from './Avatar/Avatar'
